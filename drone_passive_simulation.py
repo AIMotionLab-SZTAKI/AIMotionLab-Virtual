4582f31e3e48881dc6488aa88105db8109320d8b
AIMotionLab <aimotionlab.sztaki@gmail.com>
2022-11-02 15:23:40 +0100

    new class for passive display

--- drone_passive_simulation.py
+++ drone_passive_simulation.py
@@ -9,148 +9,160 @@ import time
 import reloadScene
 import mujocoHelper
 
-cam = mujoco.MjvCamera()
-mouse_right_btn_down = False
-prev_x, prev_y = 0.0, 0.0
+MAX_DRONE_NUM = 2
+
+class PassiveDisplay:
+    cam = mujoco.MjvCamera()
+    camFollow = mujoco.MjvCamera()
+    activeCam = cam
+    mouse_right_btn_down = False
+    prev_x, prev_y = 0.0, 0.0
+    followed_drone_ID = 0
+
+    def __init__(self):
+        # Connect to optitrack
+        self.mc = motioncapture.MotionCaptureOptitrack("192.168.1.141")
+
+        self.t1 = time.time()
+
+        # Reading model data
+        print(f'Working directory:  {os.getcwd()}\n')
+
+        xmlFileName = "testEnvironment.xml"
+
+        self.model = mujoco.MjModel.from_xml_path(xmlFileName)
+
+        self.data = mujoco.MjData(self.model)
+
+        hospitalPos, hospitalQuat, postOfficePos, postOfficeQuat = reloadScene.loadBuildingData("building_positions.txt")
+        pole1Pos, pole1Quat, pole2Pos, pole2Quat, pole3Pos, pole3Quat, pole4Pos, pole4Quat = reloadScene.loadPoleData("pole_positions.txt")
+
+        reloadScene.setBuildingData(self.model, hospitalPos, hospitalQuat, "hospital")
+        reloadScene.setBuildingData(self.model, postOfficePos, postOfficeQuat, "post_office")
+
+        reloadScene.setBuildingData(self.model, pole1Pos, pole1Quat, "pole1")
+        reloadScene.setBuildingData(self.model, pole2Pos, pole2Quat, "pole2")
+        reloadScene.setBuildingData(self.model, pole3Pos, pole3Quat, "pole3")
+        reloadScene.setBuildingData(self.model, pole3Pos, pole3Quat, "pole4")
+
+        # Initialize the library
+        if not glfw.init():
+            return
+
+        # Create a windowed mode window and its OpenGL context
+        self.window = glfw.create_window(1280, 720, "Optitrack Scene", None, None)
+        if not self.window:
+            glfw.terminate()
+            return
+
+        # Make the window's context current
+        glfw.make_context_current(self.window)
+        # setup mouse callbacks
+        glfw.set_scroll_callback(self.window, PassiveDisplay.zoom)
+        glfw.set_mouse_button_callback(self.window, PassiveDisplay.mouse_button_callback)
+        glfw.set_cursor_pos_callback(self.window, PassiveDisplay.mouse_move_callback)
+        glfw.set_key_callback(self.window, PassiveDisplay.key_callback)
+
+
+        # initialize visualization data structures
+        PassiveDisplay.cam.azimuth, PassiveDisplay.cam.elevation = 180, -30
+        PassiveDisplay.cam.lookat, PassiveDisplay.cam.distance = [0, 0, 0], 5
+        PassiveDisplay.camFollow.azimuth, PassiveDisplay.camFollow.elevation = 180, -30
+        PassiveDisplay.camFollow.lookat, PassiveDisplay.camFollow.distance = [0, 0, 0], 1
+
+        self.pert = mujoco.MjvPerturb()
+        self.opt = mujoco.MjvOption()
+        self.scn = mujoco.MjvScene(self.model, maxgeom=50)
+        self.con = mujoco.MjrContext(self.model, mujoco.mjtFontScale.mjFONTSCALE_100)
+
+        #print(self.data.qpos.size)
+
+    def run(self):
+        ## To obtain inertia matrix
+        mujoco.mj_step(self.model, self.data)
+
+        while not glfw.window_should_close(self.window):
+            # getting data from optitrack server
+            self.mc.waitForNextFrame()
+            for name, obj in self.mc.rigidBodies.items():
+
+                # have to put rotation.w to the front because the order is different
+                drone_orientation = [obj.rotation.w, obj.rotation.x, obj.rotation.y, obj.rotation.z]
+
+                if name == 'cf1':
+                    mujocoHelper.update_drone(self.data, 0, obj.position, drone_orientation)
+
+                if name == 'cf2':
+                    mujocoHelper.update_drone(self.data, 1, obj.position, drone_orientation)
+
+            if PassiveDisplay.activeCam == PassiveDisplay.camFollow:
+                mujocoHelper.update_follow_cam(self.data.qpos, PassiveDisplay.followed_drone_ID, PassiveDisplay.camFollow)
+
+            mujoco.mj_step(self.model, self.data, 1)
+            viewport = mujoco.MjrRect(0, 0, 0, 0)
+            viewport.width, viewport.height = glfw.get_framebuffer_size(self.window)
+            mujoco.mjv_updateScene(self.model, self.data, self.opt, pert=None, cam=PassiveDisplay.activeCam, catmask=mujoco.mjtCatBit.mjCAT_ALL,
+                                   scn=self.scn)
+            mujoco.mjr_render(viewport, self.scn, self.con)
+
+            glfw.swap_buffers(self.window)
+            glfw.poll_events()
+
+    def mouse_button_callback(window, button, action, mods):
+
+        if button == glfw.MOUSE_BUTTON_LEFT and action == glfw.PRESS:
+
+            PassiveDisplay.prev_x, PassiveDisplay.prev_y = glfw.get_cursor_pos(window)
+            PassiveDisplay.mouse_right_btn_down = True
+
+        elif button == glfw.MOUSE_BUTTON_LEFT and action == glfw.RELEASE:
+            PassiveDisplay.mouse_right_btn_down = False
+            pass
+
+    def mouse_move_callback(window, xpos, ypos):
+        if not PassiveDisplay.mouse_right_btn_down:
+            return
+        if PassiveDisplay.activeCam != PassiveDisplay.cam:
+            return
+
+        x, y = glfw.get_cursor_pos(window)
+
+        dx = x - PassiveDisplay.prev_x
+        dy = y - PassiveDisplay.prev_y
+
+        PassiveDisplay.cam.azimuth -= dx / 10
+        PassiveDisplay.cam.elevation -= dy / 10
+
+        PassiveDisplay.prev_x = x
+        PassiveDisplay.prev_y = y
+
+    def zoom(window, x, y):
+        PassiveDisplay.activeCam.distance -= 0.2 * y
+
+
+    def key_callback(window, key, scancode, action, mods):
+
+        if key == glfw.KEY_TAB and action == glfw.PRESS:
+            PassiveDisplay.change_cam()
+        elif key == glfw.KEY_SPACE and action == glfw.PRESS:
+            if PassiveDisplay.activeCam == PassiveDisplay.camFollow:
+                if PassiveDisplay.followed_drone_ID + 1 == MAX_DRONE_NUM:
+                    PassiveDisplay.followed_drone_ID = 0
+                else:
+                    PassiveDisplay.followed_drone_ID += 1
+
+
+    def change_cam():
+        if PassiveDisplay.activeCam == PassiveDisplay.cam:
+            PassiveDisplay.activeCam = PassiveDisplay.camFollow
+        else:
+            PassiveDisplay.activeCam = PassiveDisplay.cam
+
 
 def main():
-    # Connect to optitrack
-    mc = motioncapture.MotionCaptureOptitrack("192.168.1.141")
+    display = PassiveDisplay()
+    display.run()
 
-    t1 = time.time()
-
-    # Reading model data
-    print(f'Working directory:  {os.getcwd()}\n')
-
-    xmlFileName = "testEnvironment.xml"
-
-    model = mujoco.MjModel.from_xml_path(xmlFileName)
-
-    data = mujoco.MjData(model)
-
-    hospitalPos, hospitalQuat, postOfficePos, postOfficeQuat = reloadScene.loadBuildingData("building_positions.txt")
-    pole1Pos, pole1Quat, pole2Pos, pole2Quat, pole3Pos, pole3Quat, pole4Pos, pole4Quat = reloadScene.loadPoleData("pole_positions.txt")
-
-    reloadScene.setBuildingData(model, hospitalPos, hospitalQuat, "hospital")
-    reloadScene.setBuildingData(model, postOfficePos, postOfficeQuat, "post_office")
-
-    reloadScene.setBuildingData(model, pole1Pos, pole1Quat, "pole1")
-    reloadScene.setBuildingData(model, pole2Pos, pole2Quat, "pole2")
-    reloadScene.setBuildingData(model, pole3Pos, pole3Quat, "pole3")
-    reloadScene.setBuildingData(model, pole3Pos, pole3Quat, "pole4")
-
-    # Initialize the library
-    if not glfw.init():
-        return
-
-    # Create a windowed mode window and its OpenGL context
-    window = glfw.create_window(1280, 720, "Optitrack Scene", None, None)
-    if not window:
-        glfw.terminate()
-        return
-
-    # Make the window's context current
-    glfw.make_context_current(window)
-    # setup mouse callbacks
-    glfw.set_scroll_callback(window, zoom)
-    glfw.set_mouse_button_callback(window, mouse_button_callback)
-    glfw.set_cursor_pos_callback(window, mouse_move_callback)
-
-
-    # initialize visualization data structures
-    cam.azimuth, cam.elevation = 180, -30
-    cam.lookat, cam.distance = [0, 0, 0], 5
-
-    pert = mujoco.MjvPerturb()
-    opt = mujoco.MjvOption()
-    scn = mujoco.MjvScene(model, maxgeom=50)
-    con = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_100)
-
-    ## To obtain inertia matrix
-    mujoco.mj_step(model, data)
-    #print(data.qpos)
-
-    while not glfw.window_should_close(window):
-        mc.waitForNextFrame()
-        for name, obj in mc.rigidBodies.items():
-            if name == 'cf4':
-
-                pos_message = "{:.5f}".format(obj.position[0]) + "," + "{:.5f}".format(obj.position[1]) + "," + \
-                              "{:.5f}".format(obj.position[2])
-                t2 = time.time()-t1
-
-                # have to put rotation.w to the front because the order is different
-                drone_orientation = [obj.rotation.w, obj.rotation.x, obj.rotation.y, obj.rotation.z]
-                update_drone(data, 0, obj.position, drone_orientation)
-
-            if name == 'cf1':
-
-                pos_message = "{:.5f}".format(obj.position[0]) + "," + "{:.5f}".format(obj.position[1]) + "," + \
-                              "{:.5f}".format(obj.position[2])
-                t2 = time.time()-t1
-
-                # have to put rotation.w to the front because the order is different
-                drone_orientation = [obj.rotation.w, obj.rotation.x, obj.rotation.y, obj.rotation.z]
-                mujocoHelper.update_drone(data, 1, obj.position, drone_orientation)
-
-
-        mujoco.mj_step(model, data, 1)
-        viewport = mujoco.MjrRect(0, 0, 0, 0)
-        viewport.width, viewport.height = glfw.get_framebuffer_size(window)
-        mujoco.mjv_updateScene(model, data, opt, pert=None, cam=cam, catmask=mujoco.mjtCatBit.mjCAT_ALL,
-                               scn=scn)
-        mujoco.mjr_render(viewport, scn, con)
-
-        glfw.swap_buffers(window)
-        glfw.poll_events()
-        #print(mouse_right_btn_down)
-
-
-                #print("--------------------")
-                #print("Name:", name)
-                #print("Time:", t2, "sec")
-                #print("Position:", obj.position)
-                #print("Rotation:", obj.rotation.x, obj.rotation.y, obj.rotation.z, obj.rotation.w)
-                #print("Distance:", float(recived))
-
-
-def mouse_button_callback(window, button, action, mods):
-    global mouse_right_btn_down
-    if button == glfw.MOUSE_BUTTON_LEFT and action == glfw.PRESS:
-        global prev_x, prev_y
-        prev_x, prev_y = glfw.get_cursor_pos(window)
-        mouse_right_btn_down = True
-    elif button == glfw.MOUSE_BUTTON_LEFT and action == glfw.RELEASE:
-        mouse_right_btn_down = False
-        pass
-
-
-def mouse_move_callback(window, xpos, ypos):
-    if not mouse_right_btn_down:
-        return
-
-    global prev_x, prev_y
-    x, y = glfw.get_cursor_pos(window)
-
-    dx = x - prev_x
-    dy = y - prev_y
-
-    cam.azimuth -= dx / 10
-    cam.elevation -= dy / 10
-
-    #print("dx: " + dx)
-    #print("dy: " + dy)
-
-    #print("azi: " + cam.azimuth)
-    #print("elev: " + cam.elevation)
-
-    prev_x = x
-    prev_y = y
-
-    #print(dx, dy)
-
-def zoom(window, x, y):
-    cam.distance -= 0.2 * y
 
 if __name__ == '__main__':
     main()
--- mujocoHelper.py
+++ mujocoHelper.py
@@ -10,12 +10,11 @@ def update_drone(data, droneID, position, orientation):
     droneID should be 0 for the first drone defined in the xml, 1 for the second etc.
     """
     startIdx = droneID * 7
-    endIdx = startIdx + 7
-    if endIdx >= data.qpos.size:
-        print("Drone id out of bounds of data.qpos. (Not enough drones defined in the xml)")
+    if startIdx + 6 >= data.qpos.size:
+        print("Drone id: " + str(droneID) + " out of bounds of data.qpos. (Not enough drones defined in the xml)")
         return
     data.qpos[startIdx:startIdx + 3] = position
-    data.qpos[startIdx + 3:endIdx] = orientation
+    data.qpos[startIdx + 3:startIdx + 7] = orientation
 
 
 def euler_from_quaternion(x, y, z, w):
@@ -39,3 +38,19 @@ def euler_from_quaternion(x, y, z, w):
     yaw_z = math.atan2(t3, t4)
 
     return roll_x, pitch_y, yaw_z  # in radians
+
+def update_follow_cam(qpos, droneID, cam):
+    """
+    updates the position and orientation of the camera that follows the drone from behind
+    qpos is the array in which the position and orientation of all the drones are stored
+    """
+
+    startIdx = droneID * 7
+    position = qpos[startIdx:startIdx + 3]
+    orientation = qpos[startIdx + 3:startIdx + 7]
+
+    roll_x, pitch_y, yaw_z = euler_from_quaternion(orientation[0], orientation[1], orientation[2], orientation[3])
+
+    cam.lookat = position
+    cam.azimuth = -math.degrees(roll_x)
+    cam.elevation = -math.degrees(pitch_y) - 20

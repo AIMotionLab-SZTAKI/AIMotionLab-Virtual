c44a21fc08a2df423ad782c3f8ae68b03de1d149
AIMotionLab <aimotionlab.sztaki@gmail.com>
2022-11-10 13:48:31 +0100

    ET.indent not supported on python 3.8

--- xml_generator.py
+++ xml_generator.py
@@ -145,7 +145,7 @@ class SceneXmlGenerator:
     def save_xml(self, file_name):
         
         tree = ET.ElementTree(self.root)
-        ET.indent(tree, space="\t", level=0)
+        # ET.indent(tree, space="\t", level=0) # uncomment this if python version >= 3.9
         tree.write(file_name)
 
 

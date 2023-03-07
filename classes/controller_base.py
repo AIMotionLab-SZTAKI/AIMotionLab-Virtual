8b1e48d148ba0d0c89a6645e0b830484beefee04
Mate <moroknai@gmail.com>
2023-03-07 14:21:03 +0100

    create dummy trajectories for demonstration

--- classes/controller_base.py
+++ classes/controller_base.py
@@ -13,3 +13,24 @@ class ControllerBase:
         # if it's a car controller, return two floats: d, delta. They will be converted to actuator controls by the car class.
 
         raise NotImplementedError("compute_control method must be implemented")
+
+
+class DummyDroneController(ControllerBase):
+
+    def __init__(self, mass, inertia, gravity):
+        super().__init__(mass, inertia, gravity)
+
+    
+    def compute_control(self, state, setpoint, time) -> np.array:
+        return np.array((0, 0, 0, 0))
+
+class DummyCarController(ControllerBase):
+
+    def __init__(self, mass, inertia, gravity):
+        super().__init__(mass, inertia, gravity)
+    
+
+    def compute_control(self, state, setpoint, time) -> np.array:
+        d = 0
+        delta = 0
+        return np.array((d, delta))
--- classes/trajectory_base.py
+++ classes/trajectory_base.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class TrajectoryBase:
 
@@ -11,3 +11,46 @@ class TrajectoryBase:
     def evaluate(self, state, i, time, control_step) -> dict:
 
         raise NotImplementedError("evaluate method must be implemented")
+
+
+class DummyDroneTrajectory(TrajectoryBase):
+
+    def __init__(self):
+        super().__init__()
+        
+        # self.output needs to be updated and returned in evaluate()
+        # the keys of the dictionary can be changed, this is only an example
+        self.output = {
+            "load_mass" : 0.0,
+            "target_pos" : None,
+            "target_rpy" : np.zeros(3),
+            "target_vel" : np.zeros(3),
+            "target_acc" : None,
+            "target_quat" : None,
+            "target_quat_vel" : None,
+            "target_pos_load" : None
+        }
+    
+
+    def evaluate(self, state, i, time, control_step):
+
+        return self.output
+
+class DummyCarTrajectory(TrajectoryBase):
+
+    def __init__(self):
+        super().__init__()
+
+        # self.output needs to be updated and returned in evaluate()
+        # the keys of the dictionary can be changed, this is only an example
+        self.output = {
+            "target_pos" : None,
+            "target_rpy" : np.zeros(3),
+            "target_vel" : np.zeros(3),
+            "target_acc" : None,
+            "target_quat" : None,
+            "target_quat_vel" : None,
+        }
+    
+    def evaluate(self, state, i, time, control_step) -> dict:
+        return self.output

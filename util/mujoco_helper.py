0382c6337fa80ac390b3db520d879b1a9fcf0669
AIMotionLab <aimotionlab.sztaki@gmail.com>
2023-05-05 13:58:17 +0200

    add function for quaternion vector array multiplication

--- util/mujoco_helper.py
+++ util/mujoco_helper.py
@@ -157,6 +157,10 @@ def quaternion_from_euler(roll, pitch, yaw):
 
     return [qw, qx, qy, qz]
 
+def q_conjugate(q):
+    w, x, y, z = q
+    return [w, -x, -y, -z]
+
 
 def quaternion_multiply(quaternion0, quaternion1):
     """Return multiplication of two quaternions.
@@ -169,9 +173,6 @@ def quaternion_multiply(quaternion0, quaternion1):
                     -x1*z0 + y1*w0 + z1*x0 + w1*y0,
                     x1*y0 - y1*x0 + z1*w0 + w1*z0), dtype=np.float64)
 
-def q_conjugate(q):
-    w, x, y, z = q
-    return [w, -x, -y, -z]
 
 def qv_mult(q1, v1):
     """For active rotation. If passive rotation is needed, use q1 * q2 * q1^(-1)"""
@@ -181,6 +182,51 @@ def qv_mult(q1, v1):
 def qv_mult_passive(q1, v1):
     q2 = np.append(0.0, v1)
     return quaternion_multiply(quaternion_multiply(q1, q2), q_conjugate(q1))[1:]
+
+
+
+def quat_array_conjugate(quat_array):
+    quat_array[:, 1] *= -1
+    quat_array[:, 2] *= -1
+    quat_array[:, 3] *= -1
+    return quat_array
+
+def quat_array_quat_multiply(quat_array, quat):
+    
+    w0, x0, y0, z0 = quat_array[:, 0], quat_array[:, 1], quat_array[:, 2], quat_array[:, 3]
+    w1, x1, y1, z1 = quat
+    return np.stack((-x1*x0 - y1*y0 - z1*z0 + w1*w0,
+                      x1*w0 + y1*z0 - z1*y0 + w1*x0,
+                     -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+                      x1*y0 - y1*x0 + z1*w0 + w1*z0), axis=1)
+
+def quat_quat_array_multiply(quat, quat_array):
+
+    w0, x0, y0, z0 = quat
+    w1, x1, y1, z1 = quat_array[:, 0], quat_array[:, 1], quat_array[:, 2], quat_array[:, 3]
+    return np.stack((-x1*x0 - y1*y0 - z1*z0 + w1*w0,
+                      x1*w0 + y1*z0 - z1*y0 + w1*x0,
+                     -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+                      x1*y0 - y1*x0 + z1*w0 + w1*z0), axis=1)
+
+
+def quat_array_quat_array_multiply(quat_array0, quat_array1):
+
+    w0, x0, y0, z0 = quat_array0[:, 0], quat_array0[:, 1], quat_array0[:, 2], quat_array0[:, 3]
+    w1, x1, y1, z1 = quat_array1[:, 0], quat_array1[:, 1], quat_array1[:, 2], quat_array1[:, 3]
+    return np.stack((-x1*x0 - y1*y0 - z1*z0 + w1*w0,
+                      x1*w0 + y1*z0 - z1*y0 + w1*x0,
+                     -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+                      x1*y0 - y1*x0 + z1*w0 + w1*z0), axis=1)
+
+def quat_vect_array_mult(q, v_array):
+    q_array = np.append(np.zeros((v_array.shape[0], 1)), v_array, axis=1)
+    return quat_quat_array_multiply(q_conjugate(q), quat_array_quat_multiply(q_array, q))[:, 1:]
+
+
+def quat_vect_array_mult_passive(q, v_array):
+    q_array = np.append(np.zeros((v_array.shape[0], 1)), v_array, axis=1)
+    return quat_array_quat_multiply(quat_quat_array_multiply(q, q_array), q_conjugate(q))[:, 1:]
 
 
 def euler_rad_to_euler_deg(array_3elem):

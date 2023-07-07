500405b7530b3f8c04045f876e531d707c92019b
Mate <moroknai@gmail.com>
2023-07-07 13:04:15 +0200

    add Bicycle for the tutorial

---
+++ classes/bicycle.py
@@ -0,0 +1,31 @@
+import mujoco
+from classes.moving_object import MovingObject
+
+class Bicycle(MovingObject):
+
+    def __init__(self, model: mujoco.MjModel, data: mujoco.MjData, name_in_xml):
+        
+        super().__init__(model, name_in_xml)
+
+        self.actr = data.actuator(name_in_xml + "_actr")
+        self.ctrl = self.actr.ctrl
+        self.sensor_velocimeter = data.sensor(name_in_xml + "_velocimeter").data
+    
+    def update(self, i, control_step):
+        
+        if self.controllers is not None:
+            ctrl = self.controllers[0].compute_control(i)
+
+            self.ctrl[0] = ctrl
+
+
+from classes.controller_base import ControllerBase
+
+class BicycleController(ControllerBase):
+
+    def __init__(self):
+        pass
+    
+    def compute_control(self, i):
+        
+        return 0.1

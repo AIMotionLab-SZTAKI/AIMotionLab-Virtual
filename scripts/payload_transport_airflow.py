49503ef2356abbaf30aefbba44199d189b0bf7d5
mkoroknai <mate.koroknai93@gmail.com>
2023-09-14 15:04:32 +0200

    test different hooklengths

--- scripts/payload_transport_airflow.py
+++ scripts/payload_transport_airflow.py
@@ -3,6 +3,8 @@ from classes.active_simulation import ActiveSimulator
 from util import xml_generator
 from classes.drone_classes.hooked_drone_trajectory import HookedDroneTrajectory
 from classes.drone_classes.drone_geom_control import GeomControl
+from classes.drone_classes.hooked_drone_lq_control import LqrLoadControl
+from classes.drone import BUMBLEBEE_PROP
 import numpy as np
 import matplotlib.pyplot as plt
 from classes.airflow_sampler import AirflowSampler
@@ -24,17 +26,19 @@ if __name__ == '__main__':
     xml_path = os.path.join(abs_path, "..", "xml_models")
     xmlBaseFileName = "scene_base.xml"
     save_filename = "built_scene.xml"
+    
+    rod_length = float(BUMBLEBEE_PROP.ROD_LENGTH.value)
 
     # Set scenario parameters
     drone_init_pos = np.array([-0.76, 1.13, 1, 0])  # initial drone position and yaw angle
-    load_init_pos = np.array([0, -1, 0.6])  # TODO: Do some transformations in z direction
-    load_target_pos = np.array([0.76, 1.13, 0.62])
-    load_mass = 0.08
+    load_init_pos = np.array([-0.01, -1, 0.1995 + rod_length])  # TODO: Do some transformations in z direction
+    load_target_pos = np.array([0.76, 1.13, 0.19 + rod_length])
+    load_mass = 0.03
+
 
     # create xml with a drone and a car
     scene = xml_generator.SceneXmlGenerator(xmlBaseFileName)
-    drone0_name = scene.add_drone(np.array2string(drone_init_pos[0:3])[1:-2], "1 0 0 0", RED_COLOR, True, "bumblebee",
-                                  True, 2)
+    drone0_name = scene.add_drone(np.array2string(drone_init_pos[0:3])[1:-2], "1 0 0 0", RED_COLOR, True, "bumblebee", True, 1)
     payload0_name = scene.add_payload(np.array2string(load_init_pos)[1:-2], ".05 .05 .025", str(load_mass), "1 0 0 0", BLUE_COLOR)
 
     # saving the scene as xml so that the simulator can load it
@@ -57,7 +61,8 @@ if __name__ == '__main__':
     drone0_trajectory = HookedDroneTrajectory()
     drone0_trajectory.set_control_step(control_step)
     drone0_trajectory.set_rod_length(drone0.rod_length)
-    drone0_controller = GeomControl(drone0.mass, drone0.inertia, simulator.gravity)
+    drone0_controller = LqrLoadControl(drone0.mass, drone0.inertia, simulator.gravity)
+    drone0_controller.L = rod_length
 
     drone0_controllers = [drone0_controller]
 
@@ -70,8 +75,7 @@ if __name__ == '__main__':
     velocity_lut_path = os.path.join(abs_path, "..", "airflow_data", "airflow_luts", "flow_velocity_shifted.txt")
 
     airflow_sampl = AirflowSampler(pressure_lut_path, drone0, velocity_lut_path)
-    payload0.set_top_mesh(30, 30)
-    payload0.set_side_mesh(30, 30, 30)
+    payload0.create_surface_mesh(0.00001)
 
     # start simulation
     sensor_data = []
@@ -79,16 +83,38 @@ if __name__ == '__main__':
     drone0.qvel[0] = 0
     drone0.qvel[1] = 0
 
+    ctrl0_max = 0
+    ctrl1_max = 0
+    ctrl2_max = 0
+    ctrl3_max = 0
+
+    def is_greater_than(new_value, current_max):
+
+        if new_value > current_max:
+            return new_value
+        
+        return current_max
+
+
     # Plan trajectory
-    drone0_trajectory.construct(drone_init_pos, load_init_pos - np.array([0.001, 0, 0.005]), load_target_pos, load_mass)
+    drone0_trajectory.construct(drone_init_pos, load_init_pos - np.array([-.02, 0, rod_length + 0.04]), load_target_pos - np.array([0, 0, rod_length + .05]), load_mass)
 
     while not simulator.glfw_window_should_close():
         simulator.update()
 
         force, torque = airflow_sampl.generate_forces_opt(payload0)
         payload0.set_force_torque(force, torque)
+        
+        ctrl0_max = is_greater_than(drone0.ctrl0[0], ctrl0_max)
+        ctrl1_max = is_greater_than(drone0.ctrl1[0], ctrl1_max)
+        ctrl2_max = is_greater_than(drone0.ctrl2[0], ctrl2_max)
+        ctrl3_max = is_greater_than(drone0.ctrl3[0], ctrl3_max)
 
     simulator.close()
 
+    print(np.max(ctrl0_max))
+    print(np.max(ctrl1_max))
+    print(np.max(ctrl2_max))
+    print(np.max(ctrl3_max))
 
     plot_payload_and_airflow_volume(payload0, airflow_sampl)

d43272929d7adb0dadc008c893676db66e90662c
AIMotionLab <aimotionlab.sztaki@gmail.com>
2024-01-08 13:42:19 +0100

    add radar field height scale

--- scripts/terrain_display.py
+++ scripts/terrain_display.py
@@ -6,6 +6,7 @@ from aiml_virtual.object.drone import BUMBLEBEE_PROP, DRONE_TYPES
 from aiml_virtual.object import parseMovingObjects
 from aiml_virtual.trajectory.drone_keyboard_trajectory import DroneKeyboardTraj
 import numpy as np
+from aiml_virtual.util.mujoco_helper import Radar
 import math
 
 
@@ -25,19 +26,22 @@ xml_path = os.path.join(abs_path, "..", "xml_models")
 xml_base_file_name = "scene_base_terrain.xml"
 save_filename = "built_scene.xml"
 
-radar_a = 800.
-radar_exp = 5.
-radar_rres = 60
-radar_res = 30
+radar0 = Radar(np.array((-2400, 2400, 1000)), 800., 1.5, 150, 90, height_scale=.5, tilt=-0.1)
+#radar1 = Radar(np.array((20, 38, 21)), 20., 2.5, 50, 60)
+#radar2 = Radar(np.array((-36, -38, 10)), 15., 2.5, 50, 60)
 
-drone0_initpos = np.array((radar_pos[0] + (2 * radar_a) + 1., radar_pos[1], hover_height))
+hover_height = radar0.pos[2] + 100.
+#radars = [radar0, radar1, radar2]
+radars = [radar0]
+
+drone0_initpos = np.array((radar0.pos[0] - (2 * radar0.a) - 1.0, radar0.pos[1], hover_height))
 
 
 scene = SceneXmlGenerator(xml_base_file_name)
 drone0_name = scene.add_drone(np.array2string(drone0_initpos)[1:-1], "1 0 0 0", BLUE, DRONE_TYPES.BUMBLEBEE)
-scene.add_radar_field(np.array2string(radar_pos)[1:-1], ".5 .5 .5 0.5", radar_a, radar_exp, radar_rres, radar_res, sampling="curv")
-#scene.add_radar_field("-1.6 10.35 6.5", "0.1 0.8 0.1 1.0")
-#scene.add_radar_field("2.11 -7.65 5.40")
+
+for radar in radars:
+    scene.add_radar_field(np.array2string(radar.pos)[1:-1], radar.color, radar.a, radar.exp, radar.rres, radar.res, radar.height_scale, radar.tilt, sampling="curv")
 
 scene.save_xml(os.path.join(xml_path, save_filename))
 
@@ -59,61 +63,13 @@ simulator.onBoard_elev_offset = 15
 
 trajectory = DroneKeyboardTraj(0, drone0_initpos)
 
-simulator.set_key_up_callback(trajectory.up_press)
-simulator.set_key_up_release_callback(trajectory.up_release)
-simulator.set_key_down_callback(trajectory.down_press)
-simulator.set_key_down_release_callback(trajectory.down_release)
-simulator.set_key_left_callback(trajectory.left_press)
-simulator.set_key_left_release_callback(trajectory.left_release)
-simulator.set_key_right_callback(trajectory.right_press)
-simulator.set_key_right_release_callback(trajectory.right_release)
-simulator.set_key_a_callback(trajectory.a_press)
-simulator.set_key_a_release_callback(trajectory.a_release)
-simulator.set_key_s_callback(trajectory.s_press)
-simulator.set_key_s_release_callback(trajectory.s_release)
-simulator.set_key_d_callback(trajectory.d_press)
-simulator.set_key_d_release_callback(trajectory.d_release)
-simulator.set_key_w_callback(trajectory.w_press)
-simulator.set_key_w_release_callback(trajectory.w_release)
+trajectory.set_key_callbacks(simulator)
 
 d0 = simulator.get_MovingObject_by_name_in_xml(drone0_name)
 d0.set_controllers([LqrControl(d0.mass, d0.inertia, simulator.gravity)])
 d0.set_trajectory(trajectory)
 
 simulator.set_key_t_callback(d0.toggle_sphere_alpha)
-
-def scale_sphere(drone, simulator):
-
-    drone0_pos = drone.get_state()["pos"]
-
-    elev_rad = math.radians(simulator.activeCam.elevation)
-    azim_rad = math.radians(simulator.activeCam.azimuth)
-
-    a = simulator.activeCam.distance * math.cos(elev_rad)
-    dz = simulator.activeCam.distance * math.sin(elev_rad)
-
-    dx = a * math.cos(azim_rad)
-    dy = a * math.sin(azim_rad)
-
-    c = simulator.activeCam.lookat - np.array((dx, dy, dz))
-
-    d_cs = math.sqrt((c[0] - drone0_pos[0])**2 + (c[1] - drone0_pos[1])**2 + (c[2] - drone0_pos[2])**2)
-
-    drone.set_sphere_size(d_cs / 100.0)
-
-
-def is_point_inside_lobe(point, radar_center, a, exponent):
-
-    d = math.sqrt((radar_center[0] - point[0])**2 + (radar_center[1] - point[1])**2)
-
-    if d <= 2 * a:
-
-        z_lim = a * math.sin(math.acos((a - d) / a)) * math.sin(math.acos((a - d) / a) / 2.0)**exponent
-
-        if point[2] < radar_center[2] + z_lim and point[2] > (radar_center[2] - z_lim):
-            return True
-    
-    return False
 
 
 while not simulator.glfw_window_should_close():
@@ -123,19 +79,20 @@ while not simulator.glfw_window_should_close():
     #print(d0.xquat)
     #print(d0.state["quat"])
 
-    scale_sphere(d0, simulator)
-
-    if is_point_inside_lobe(d0_pos, radar_pos, radar_a, radar_exp):
-        simulator.append_title(" DRONE INSIDE")
+    d0.scale_sphere(simulator)
+    
+    busted = False
+    for radar in radars:
+        if radar.sees_drone(d0):
+            busted = True
+            break
+    
+    if busted:
+        simulator.append_title(" BUSTED")
         d0.set_sphere_color(np.array((.8, .2, .2)))
     else:
         simulator.reset_title()
         d0.reset_sphere_color()
-
-    #if simulator.i % 100 == 0:
-    #    print("azim: " + str(simulator.activeCam.azimuth))
-    #    print("elev: " + str(simulator.activeCam.elevation))
-    #    print()
 
 simulator.close()
 

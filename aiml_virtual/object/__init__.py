2db6f6ea0258a5b923687567b245a6fc3ccce3e8
AIMotionLab <aimotionlab.sztaki@gmail.com>
2024-01-16 14:35:37 +0100

    create separate file for radar

--- aiml_virtual/object/__init__.py
+++ aiml_virtual/object/__init__.py
@@ -4,3 +4,4 @@ import aiml_virtual.object.drone
 from aiml_virtual.object.object_parser import parseMocapObjects, parseMovingObjects
 import aiml_virtual.object.payload
 import aiml_virtual.object.moving_object
+from aiml_virtual.object.radar import Radar

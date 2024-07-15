98f04b261ee3a711cf1b4dd8752d75f4cb68595e
Krisztian Szabo <szabo.krisztian.hun@gmail.com>
2024-07-15 14:04:33 +0200

    Updated wind sampler logic

--- aiml_virtual/airflow/wind_sampler.py
+++ aiml_virtual/airflow/wind_sampler.py
@@ -3,24 +3,31 @@ import numpy as np
 
 class WindSampler:
     def __init__(self, wind_data_filename):
-        self._cube_size = None
+        self._dimensions = np.array([6.1, 5.1, 2.7]) # sidelengths of the rectangular cuboid in meters
+        self._resolution = np.array([6100, 5100, 2700]) # the division rate per dimension (mm resolution)
+
         self._loaded_wind_velocities = None
+        self._load_wind_velocities()
 
     def _load_wind_velocities(self):
         tmp = np.loadtxt(wind_data_filename)
-        self._cube_size = int(math.pow(tmp.shape[0] + 1, 1/3))
-        self._loaded_wind_velocities = np.reshape(tmp, (self._cube_size, self._cube_size, self._cube_size))
+        self._loaded_wind_velocities = np.reshape(tmp, self._resolution)
+
+    def _get_index_from_position(self, drone_position):
+        return (drone_position * self._resolution) / self._dimensions
 
     def generate_forces(self, drone):
-        drone_speed = drone.get_qvel()
-        wind_speed = self._get_wind_speed(drone_speed)
+        drone_state = drone.get_state_copy()
+        drone_velocity = drone_state["vel"]
+        drone_position = drone_state["pos"]
+        wind_speed = self._get_wind_speed(drone_position)
         x = wind_speed - drone_speed
         alpha = np.power(10, -2) * np.array([3.3, 2.7, 2.2])
         phi = np.diag(x)
         return np.dot(phi, alpha)
 
-    def _get_wind_speed(self, drone_speed):
+    def _get_wind_speed(self, drone_position):
         """
-            we return the corresponding wind_speed from the self._loaded_wind_velocities data
+            This works if the drone's and rectangular cuboid's coordinate system align
         """
-        pass
+        return self._load_wind_velocities[self._get_index_from_position(drone_position)]

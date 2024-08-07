a8cba39347677e64881d3a4ae89f0de0609cc844
AIMotionLab <aimotionlab.sztaki@gmail.com>
2024-08-07 15:47:23 +0200

    Replanning on terrain first version, planning delay needs to be added

--- scripts/trailer_hook_up_terrain.py
+++ scripts/trailer_hook_up_terrain.py
@@ -70,6 +70,8 @@ terrain0 = scene.add_moving_terrain("0 0 0")
 terrain1 = scene.add_moving_terrain("0 0 0")
 terrain2 = scene.add_moving_terrain("0 0 0")
 terrain3 = scene.add_moving_terrain("0 0 0")
+terrain4 = scene.add_moving_terrain("0 0 0")
+terrain5 = scene.add_moving_terrain("0 0 0")
 
 # saving the scene as xml so that the simulator can load it
 scene.save_xml(os.path.join(xml_path, save_filename))
@@ -80,7 +82,8 @@ virt_parsers = [parseMovingObjects]
 control_step, graphics_step = 0.025, 0.05
 xml_filename = os.path.join(xml_path, save_filename)
 
-bb_trajectory = HookedDroneNLTrajectory(plot_trajs=False)
+replanning_timestep = 1.5
+bb_trajectory = HookedDroneNLAdaptiveTrajectory(replanning_timestep=replanning_timestep, plot_trajs=False)
 bb_trajectory.set_control_step(control_step)
 bb_controller = LtvLqrLoadControl([0.605], [1.5e-3, 1.5e-3, 2.6e-3], [0, 0, -9.81])
 
@@ -91,11 +94,11 @@ predictor.trailer_top_plate_height = car_pos[2] + 0.067
 init_state = np.hstack((car_pos, np.fromstring(car_quat, sep=" "),
                                      np.zeros(6), 0, 0, 0, 0,
                                      payload_pos, np.fromstring(payload_quat, sep=" "), np.zeros(3)))
-load_init_pos, load_init_vel, load_init_yaw, load_yaw_rel = predictor.simulate(init_state, 0, 15)
+load_init_pos, load_init_vel, load_init_yaw, load_yaw_rel = predictor.simulate(init_state, 0, 15, num_ter=0)
 
 # Plan trajectory
 bb_trajectory.construct(drone_init_pos[0:3]-np.array([0, 0, 0.4]), drone_init_pos[3], [load_init_pos, load_init_vel],
-                        [load_init_yaw, load_yaw_rel], load_target_pos, 0, load_mass, grasp_speed=1.5)
+                        [load_init_yaw, load_yaw_rel], load_target_pos, 0, load_mass, grasp_speed=1.3)
 
 # Compute control gains
 bb_controller.setup_hook_up(bb_trajectory, hook_mass=0.02, payload_mass=load_mass)
@@ -104,7 +107,8 @@ bb_controller.setup_hook_up(bb_trajectory, hook_mass=0.02, payload_mass=load_mas
 #rec_interval=[0,12]
 rec_interval = None # no video capture
 
-simulator = ActiveSimulator(xml_filename, rec_interval, control_step, graphics_step, window_size=[3840, 2160])
+simulator = ActiveSimulator(xml_filename, rec_interval, control_step, graphics_step, window_size=[3840, 2160],
+                            with_graphics=True)
 #simulator.cam.azimuth = -45
 #simulator.cam.elevation = -25
 #simulator.cam.lookat, simulator.cam.distance = [0, 0, .5], 4
@@ -126,23 +130,62 @@ bb = simulator.get_MovingObject_by_name_in_xml(bb_name)
 bb.set_controllers([bb_controller])
 bb.set_trajectory(bb_trajectory)
 
+payload = simulator.get_MovingObject_by_name_in_xml(payload_name)
+
+replanning_cycle = int(replanning_timestep / control_step)
+num_traj_to_compute = 1
+
 scenario_duration = bb_trajectory.segment_times[-1]
 print(scenario_duration)
 while not simulator.should_close(scenario_duration):
     simulator.update()
+    # Update terrain
     num_ter = 0
-    if simulator.time > 0:
+    if simulator.time > 1:
         num_ter = 1
     if simulator.time > 2:
         num_ter = 2
-    if simulator.time > 5:
+    if simulator.time > 4:
         num_ter = 3
-    for i in range(4):
+    if simulator.time > 6:
+        num_ter = 4
+    if simulator.time > 8:
+        num_ter = 5
+    for i in range(6):
         if i == num_ter:
             simulator.data.mocap_pos[i][-1] = 0
         else:
-            simulator.data.mocap_pos[i][-1] = -1
+            simulator.data.mocap_pos[i][-1] = -10
     #if simulator.i == int(5 / control_step):
     #    simulator.pause()
+    # Replan trajectory
+    if simulator.i % replanning_cycle == 0 and num_traj_to_compute < len(bb_trajectory.replanner.planners):
+        simulator.pause()
+        # This part should be executed in parallel
+        # get qpos and qvel of car + trailer
+        #car_to_rod = car.data.joint("car_to_rod")  # ball joint
+        #rod_yaw = Rotation.from_quat(np.roll(car_to_rod.qpos, -1)).as_euler('xyz')[2]
+        #rod_yaw_rate = car_to_rod.qvel[2]
+        #front_to_rear = car.data.joint("front_to_rear")  # hinge joint
+        #car_trailer_state = np.hstack((car.joint.qpos, car.joint.qvel, rod_yaw, rod_yaw_rate, front_to_rear.qpos, front_to_rear.qvel, 
+        #                               payload.sensor_posimeter, payload.sensor_orimeter, payload.sensor_velocimeter))
+        # simulate car + trailer
+        pred_start = time.time()
+        load_init_pos_new, load_init_vel_new, load_init_yaw_new, _ = predictor.simulate(init_state, 0, 15, num_ter=num_ter)
+        pred_end = time.time()
+        print(f"Prediction time: {pred_end-pred_start:.3f} s")
+        
+        bb_trajectory.replan(num_traj_to_compute, load_init_pos_new, load_init_vel_new, load_init_yaw_new)
+        replan_end = time.time()
+        print(f"Replan time: {replan_end-pred_end:.3f} s")
+        # output: bb_trajectory.replanner.planners[num_traj] is optimized
+        num_traj_to_switch = num_traj_to_compute
+        num_traj_to_compute += 1
+    #if simulator.i == activate_timestep:
+        bb_trajectory.switch(num_traj_to_switch)
+        bb_controller.setup_hook_up(bb_trajectory, hook_mass=0.001, payload_mass=load_mass)
+        lqr_end = time.time()
+        print(f"LQR time: {lqr_end-replan_end:.3f} s")
+        simulator.unpause()
 
 simulator.close()

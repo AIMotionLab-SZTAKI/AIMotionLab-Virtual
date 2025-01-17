e4840fdb27573014f3056deb78dc6e98393f7d36
AIMotionLab <aimotionlab.sztaki@gmail.com>
2025-01-17 11:24:18 +0100

    Removed outdated todo.

--- aiml_virtual/simulator.py
+++ aiml_virtual/simulator.py
@@ -257,10 +257,6 @@ class Simulator:
         """
         Tick the simulation: call every process that needs to be called. E.g.: the physics process always gets called,
         but if a process has 1/10th the frequency of the physics process, it only gets called every 10th round.
-
-        .. todo::
-            Display the frequency of the processes, particularly update, to make sure we're not lagging behind.
-
         """
         for process in self.processes.values():
             interval = max(1, math.ceil((1 / process.target_frequency) / self.timestep))

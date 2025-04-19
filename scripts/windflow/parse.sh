9774f06ec0beb385edef5b0eb4ca39098e2f9f93
Krisztian Szabo <szabo.krisztian.hun@gmail.com>
2025-04-19 21:34:54 +0200

    Dockerfile created, docker container is working with the whole env

--- scripts/windflow/parse.sh
+++ scripts/windflow/parse.sh
@@ -3,7 +3,7 @@
 SOURCE_XML=../../aiml_virtual/resources/xml_models/static_objects.xml
 OUTPUT_PATH=./output
 
-python ./parser/parser.py \
+python3 ./parser/parser.py \
     $SOURCE_XML \
     $OUTPUT_PATH \
     --semi-merge
--- scripts/windflow/view_objects.sh
+++ scripts/windflow/view_objects.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VIEW_SCRIPT=../examples/11_test_view_static_objects.py
-python $VIEW_SCRIPT
+python3 $VIEW_SCRIPT

b9b31998bd34bfbab082a1e18fe3eaddb3bc4384
Gaál Botond <kisgal98@gmail.com>
2025-09-17 22:29:43 +0200

    Added new viewer

---
+++ scripts/examples/11_viewer.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import pathlib
+
+# The lines under here are intended to make sure imports work, by adding parent folders to the path (i.e. the list
+# of folders where the interpreter will look for a given package when you try to import it). This is to account for
+# differences in what the interpreter identifies as your current working directory when launching these scripts
+# from the command line as regular scripts vs with the -m option vs from PyCharm, as well as the script being placed
+# in any depth of sub-sub-subfolder.
+project_root = pathlib.Path(__file__).parents[0]
+sys.path.append(project_root.resolve().as_posix())  # add the folder this file is in to path
+# until we meet the "aiml_virtual" package, keep adding the current folder to the path and then changing folder into
+# the parent folder
+while "aiml_virtual" not in [f.name for f in  project_root.iterdir()]:
+    project_root = project_root.parents[0]
+    sys.path.append(project_root.resolve().as_posix())
+
+import aiml_virtual
+from aiml_virtual.skyc_viewer.skyc_viewer import SkycViewer
+
+if __name__ == "__main__":
+    viewer = SkycViewer(os.path.join(aiml_virtual.resource_directory, "two_drones.skyc"))
+    viewer.play()
